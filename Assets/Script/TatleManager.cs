be876e95e5113d180fa190046f256426cda993b0
kikunosatoshi <kikuno.satos@gmail.com>
2026-07-12 06:59:48 +0900

    [Add,Fix]スタート画面のfadeSystem

--- Assets/Script/TatleManager.cs
+++ Assets/Script/TatleManager.cs
@@ -1,17 +1,44 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.Collections;
 public class TitleManager : MonoBehaviour
 {
+    public ButtonBlink startButtonBlink;
+    public ButtonBlink exitButtonBlink;
+    public FadeByCoroutine fade;
     // ゲーム開始
     public void StartGame()
     {
-        SceneManager.LoadScene("GameScene");
+        StartCoroutine(StartRoutine());
     }
 
     // ゲーム終了
     public void ExitGame()
     {
+        StartCoroutine(ExitRoutine());
+    }
+
+    IEnumerator StartRoutine()
+    {
+        Debug.Log("① 点滅開始");
+
+        yield return StartCoroutine(startButtonBlink.Blink());
+
+        Debug.Log("② 点滅終了");
+
+        yield return StartCoroutine(fade.Fade());
+
+        Debug.Log("③ フェード終了");
+
+        SceneManager.LoadScene("GameScene");
+    }
+
+    IEnumerator ExitRoutine()
+    {
+        yield return StartCoroutine(exitButtonBlink.Blink());
+
+        yield return StartCoroutine(fade.Fade());
+
         Application.Quit();
 
 #if UNITY_EDITOR
