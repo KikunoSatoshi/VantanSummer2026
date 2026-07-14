79c07ab545313405248fbda69318eee596488866
kikunosatoshi <kikuno.satos@gmail.com>
2026-07-14 22:56:13 +0900

    [Add]ペナルティ

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class Player : MonoBehaviour
 {
+    public EnemyColor attackColor;
     private EnemySpawner spawner;
-
+    private static bool canAttack = true;      // 攻撃できるか(staticによりASD共通で押せなくなる、static≒ゲーム全体で1つだけ存在するもの)
+    public float penaltyTime = 1f;      // ペナルティ時間
     void Start()
     {
-        spawner = FindFirstObjectByType<EnemySpawner>();
+        spawner = FindAnyObjectByType<EnemySpawner>();
     }
 
     void Update()
     {
+        if (!canAttack)
+        {
+            return;
+        }
+
         if (Keyboard.current.aKey.wasPressedThisFrame)
         {
             Attack(EnemyColor.Red);
@@ -32,9 +40,26 @@ public class Player : MonoBehaviour
     {
         Enemy enemy = spawner.GetFrontEnemy();
 
-        if (enemy != null)
+        if (enemy == null)
         {
-            enemy.TakeDamage(attackColor);
+            return;
         }
+
+        // 色が違ったらペナルティ開始
+        if (enemy.color != attackColor)
+        {
+            StartCoroutine(Penalty());
+        }
+
+        enemy.TakeDamage(attackColor);
+    }
+
+    IEnumerator Penalty()
+    {
+        canAttack = false;                  // 攻撃禁止
+
+        yield return new WaitForSeconds(penaltyTime);
+
+        canAttack = true;                   // 攻撃再開
     }
 }
